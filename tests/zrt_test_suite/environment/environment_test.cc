573b55bab01b6ffb96b2ff575d0aebdce6c55585
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2012-07-26 15:46:38 +0300

    added: gtest, zrt_test_suite

---
+++ tests/zrt_test_suite/environment/environment_test.cc
@@ -0,0 +1,40 @@
+/*
+ * Tests for zerovm network protocol layer
+ *  Author: YaroslavLitvinov
+ *  Date: 7.03.2012
+ */
+
+#include "zrt.h"
+
+#include "gtest/gtest.h"
+
+// Test harness for routines in zmq_netw.c
+class ZrtTests : public ::testing::Test {
+public:
+	ZrtTests(){}
+	~ZrtTests(){}
+	virtual void SetUp();
+	virtual void TearDown();
+};
+
+
+void ZrtTests::SetUp() {
+}
+
+void ZrtTests::TearDown() {
+}
+
+
+TEST_F(ZrtTests, TestEnvironment) {
+	//EXPECT_EQ( 0, 0 );
+}
+
+extern "C"{
+int main(int argc, char **argv, char **envp) {
+	WRITE_LOG( "main\n" );
+	//testing::InitGoogleTest(&argc, argv);
+	//return RUN_ALL_TESTS();
+	return 0;
+}
+}
+
