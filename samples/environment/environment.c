d62b3422d65e356a6bef7bf5ac4f47a9186ecb59
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2012-08-01 02:03:37 +0300

    environment enhanced tests, getenv, setenv

--- samples/environment/environment.c
+++ samples/environment/environment.c
@@ -2,17 +2,34 @@
  * demo of the new user custom attributes design
  */
 #include <stdio.h>
-#include "api/zrt.h"
+#include <stdlib.h> //getenv
+#include "zrt.h"
 
 int main(int argc, char **argv, char **envp)
 {
-  printf("command line parameters:\n");
-  while(*argv != NULL)
-    printf("\"%s\"\n", *argv++);
-    
-  printf("environment variables:\n");
-  while(*envp != NULL)
-    printf("\"%s\"\n", *envp++);
+    extern char **environ;
+    char **env = environ;
+    printf("environment variables list tests\n");
 
-  return 0;
+    printf("\nTEST1: using extern environ:\n");
+    while(*env != NULL)
+       printf("\"%s\"\n", *env++);
+
+    printf("\nTEST2: using envp:\n");
+    while(*envp != NULL)
+       printf("\"%s\"\n", *envp++);
+
+    printf("\nTEST3: using getenv:\n");
+    const char* var = NULL;
+    printf("get environment variables, by getenv:\n");
+    var = "TimeStamp";  printf("%s=%s\n", var, getenv(var) );
+    var = "SafeWords";  printf("%s=%s\n", var, getenv(var) );
+
+    printf("\nTEST4: using setenv & getenv:\n");
+    printf( "before new environment assignemnt\n" );
+    var = "Pum";
+    printf("%s=%s\n", var, getenv(var) );
+    printf( "setenv status=%d;\nafter new environment assignment\n",  setenv( "Pum", "PumPum", 1) );
+    printf("%s=%s\n", var, getenv(var) );
+    return 0;
 }
