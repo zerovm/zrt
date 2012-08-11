12f1b132526edcb20a44a49cbb40631bd8f7d9ff
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2012-08-12 02:08:26 +0300

    reqrep sample: absolute path in manifests used, sse4.1 instruction checking

--- samples/sort_paging/run_test.sh
+++ samples/sort_paging/run_test.sh
@@ -1,11 +1,11 @@
 ./genmanifest.sh
 echo ---------------------------------------------------- generating
-../../zvm/zerovm -Mgenerator.manifest -v2
+time ../../zvm/zerovm -Mgenerator.manifest -v2
 cat generator.stderr.log
 echo ---------------------------------------------------- sorting
-../../zvm/zerovm -Msort.manifest -v2
+time ../../zvm/zerovm -Msort.manifest -v2
 cat sort.stderr.log
 echo ---------------------------------------------------- testing
 ../../zvm/zerovm -Mtest.manifest -v2
-cat tester.stderr.log
+cat test.stderr.log
 
