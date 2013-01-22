e055c1ef0915fcae5767c947d88b35a04f865875
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-01-22 17:43:45 -0600

    zshell python scripting improved, additional fstab.conf added to be used with python. readme updated.

--- samples/zshell/python/scripts/hello.py
+++ samples/zshell/python/scripts/hello.py
@@ -1,4 +1,2 @@
 #python
-import sys
 print("Hello world")
-print(sys.argv)
---
+++ samples/zshell/python/scripts/tpickle.py
@@ -0,0 +1,48 @@
+#python
+import pickle
+import sys
+from io import StringIO
+
+src = StringIO()
+p = pickle.Pickler(src)
+
+def persistent_id(obj):
+    if hasattr(obj, 'x'):
+        return 'the value %d' % obj.x
+    else:
+        return None
+
+p.persistent_id = persistent_id
+
+class Integer:
+    def __init__(self, x):
+        self.x = x
+    def __str__(self):
+        return 'My name is integer %d' % self.x
+
+i = Integer(7)
+print(i)
+p.dump(i)
+
+datastream = src.getvalue()
+print(repr(datastream))
+dst = StringIO(datastream)
+
+up = pickle.Unpickler(dst)
+
+class FancyInteger(Integer):
+    def __str__(self):
+        return 'I am the integer %d' % self.x
+
+def persistent_load(persid):
+    print(persid)
+    if persid.startswith('the value '):
+        value = int(persid.split()[2])
+        return FancyInteger(value)
+    else:
+        raise pickle.UnpicklingError
+
+up.persistent_load = persistent_load
+
+j = up.load()
+print(j)
