b87c95cea78f1eb5a811acdfee766d53349b2c01
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-01-22 17:45:56 -0600

    autotest added for testing doubleslashes paths normalizing

---
+++ tests/zrt_test_suite/autotests/test_wrong_slashes_path.c
@@ -0,0 +1,23 @@
+/*
+ * normalizing wrong path testing
+ */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+#define SLASHES_PATH "//wrng"
+#define OK_PATH "/wrng"
+
+int zmain(int argc, char **argv)
+{
+    struct stat st;
+    int res;
+    int fd = open(SLASHES_PATH, O_CREAT|O_RDWR);
+    close(fd);
+    res = stat(OK_PATH, &st);
+    return res;
+}
