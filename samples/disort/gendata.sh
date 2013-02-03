7d2dab92d9fb7dc10eab7e1adba9e2aaa245a9ce
Constantine Peresypkin <constantine@litestack.com>
2013-02-03 10:48:58 -0600

    merge

--- samples/disort/gendata.sh
+++ samples/disort/gendata.sh
@@ -9,7 +9,7 @@ SRC_LAST=10
 COUNTER=$SRC_FIRST
 while [  $COUNTER -le $SRC_LAST ]; do
     echo generate data for $COUNTER node
-    ${SETARCH} ${ZEROVM} -Mmanifest/generator"$COUNTER".manifest
+    ${SETARCH} ${ZEROVM} -Mmanifest/generator"$COUNTER".manifest -e
     cat log/"$COUNTER"generator.stderr.log
     let COUNTER=COUNTER+1 
 done
