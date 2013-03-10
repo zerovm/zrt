91c49346f7e59c1e44b3171579414f70f53f1618
Constantine Peresypkin <constantine@litestack.com>
2013-03-10 13:23:07 -0500

    loglibc using zvm api directly

--- lib/libc/loglibc.c
+++ lib/libc/loglibc.c
@@ -1,26 +1,15 @@
 
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <assert.h>
 
 #include "zvm.h"
 #include "zcalls_zrt.h"
 #include "zrtlog.h"
-#include "zrt_helper_macros.h"
-#include "transparent_mount.h"
-#include "mounts_interface.h"
-#include "path_utils.h"
 
 
 static int s_debugfd = -1;
+
+
 
 int zrt_zcall_loglibc(const char *str){
     if ( s_debugfd < 0 ){
@@ -33,9 +22,10 @@ int zrt_zcall_loglibc(const char *str){
 	}
     }
 
+    
     if ( s_debugfd >0 && str ){
-	__zrt_log_write(s_debugfd, str, strlen(str), 0);
-#define ENOSYS_STR_LEN 11
-	__zrt_log_write(s_debugfd, "ENOSYS on \n", ENOSYS_STR_LEN, 0);
+        zvm_pwrite(s_debugfd, str, strlen(str), 0);
+#define SUFFIX_STR_LEN 1
+        zvm_pwrite(s_debugfd, "\n", SUFFIX_STR_LEN, 0);
     }
 }
