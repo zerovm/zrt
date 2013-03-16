4da7bf2043eb21600a53d4f28f4d629d32536a0f
Constantine Peresypkin <constantine@litestack.com>
2013-03-16 15:16:05 -0500

    libbz2 ported
    small fixes to imagemagick and pkg-config installed

--- libports/imagemagick-6.6.9.7/conf.sh
+++ libports/imagemagick-6.6.9.7/conf.sh
@@ -3,7 +3,7 @@
 ZVM_TOOLCHAIN=${ZVM_SDK_ROOT}
 export PATH=${PATH}:${ZVM_TOOLCHAIN}/bin:
 
-export CC="x86_64-nacl-gcc"
+export CC="x86_64-nacl-gcc -static"
 export CXX="x86_64-nacl-g++"
 export AR="x86_64-nacl-ar"
 export RANLIB="x86_64-nacl-ranlib"
@@ -13,12 +13,16 @@ export LDFLAGS="-static -m64 -Wl,-T${ZVM_SDK_ROOT}/x86_64-nacl/lib64/ldscripts/e
 ${ZRT_ROOT}/lib/zrt.o,-L${ZRT_ROOT}/lib,-lzrt,-lfs,-lstdc++"
 export LIBC=""
 export FREETYPE_CFLAGS="-I${ZRT_ROOT}/include/freetype2"
+export FREETYPE_LIBS="-L${ZRT_ROOT}/lib -lfreetype"
 export FONTCONFIG_CFLAGS="-I${ZRT_ROOT}/include/fontconfig"
+export FONTCONFIG_LIBS="-L${ZRT_ROOT}/lib -lfontconfig"
+export PKG_CONFIG="${ZRT_ROOT}/bin/pkg-config"
+export ac_cv_prog_freetype_config="${ZRT_ROOT}/bin/freetype-config"
 
 ./configure \
 --host=x86_64-nacl \
 --enable-static \
 --without-threads \
---without-pango \
 --disable-largefile \
+--with-fontconfig \
 --prefix=${ZRT_ROOT}
