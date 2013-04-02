c7b2e6c3c35dbb133bd129e48f912f6850c6f015
Constantine Peresypkin <constantine@litestack.com>
2013-04-02 11:01:57 -0500

    fix etag command line in dsort

--- samples/disort/gendata.sh
+++ samples/disort/gendata.sh
@@ -9,7 +9,7 @@ SRC_LAST=10
 COUNTER=$SRC_FIRST
 while [  $COUNTER -le $SRC_LAST ]; do
     echo generate data for $COUNTER node
-    ${SETARCH} ${ZEROVM} -Mmanifest/generator"$COUNTER".manifest -e
+    ${SETARCH} ${ZEROVM} -Mmanifest/generator"$COUNTER".manifest -e1
     cat log/"$COUNTER"generator.stderr.log
     let COUNTER=COUNTER+1 
 done
