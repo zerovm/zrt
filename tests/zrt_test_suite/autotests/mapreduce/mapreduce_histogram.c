e2460f2607b3d8e5bf337d7b7d901a5720a973b4
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-04-03 18:13:39 -0500

    fixes #43 autotest updated

--- tests/zrt_test_suite/autotests/mapreduce/mapreduce_histogram.c
+++ tests/zrt_test_suite/autotests/mapreduce/mapreduce_histogram.c
@@ -43,19 +43,8 @@
 	memcpy( (key_hash_p), &temphash, sizeof(hash_type) );	\
     }
 
-static char* 
-PrintableHash( char* str, const uint8_t* hash, int size){
-    snprintf(str, size*2+1, "%X", *(uint32_t*)hash);
-    return str;
-}
 
-static int 
-HashComparator(const void *h1, const void *h2){
-    if      ( *(HASH_TYPE*)h1 < *(HASH_TYPE*)h2 ) return -1;
-    else if ( *(HASH_TYPE*)h1 > *(HASH_TYPE*)h2 ) return 1;
-    else return 0;
-}
-
+/**helper functions*/
 static int
 AddItemsStrKey(Buffer* array, const char* nullstrkey, VALUE_TYPE value, HASH_TYPE hash,
 	      int duplicate_items_count){
@@ -69,15 +58,88 @@ AddItemsStrKey(Buffer* array, const char* nullstrkey, VALUE_TYPE value, HASH_TYP
     return duplicate_items_count;
 }
 
-static int
-AddItemsIntoBuffer(Buffer* array){
-    int ret=0;
-    ret+=AddItemsStrKey( array, "hello", 0xf00001, 1, 5 );
-    ret+=AddItemsStrKey( array, "pello", 0x0f0001, 2, 21 );
-    ret+=AddItemsStrKey( array, "bello", 0x00f001, 3, 56 );
-    ret+=AddItemsStrKey( array, "good",  0x000f02, 4, 56 );
-    return ret;
+
+/*Mapreduce user interface functions*/
+
+static char* 
+PrintableHash( char* str, const uint8_t* hash, int size){
+    snprintf(str, size*2+1, "%X", *(uint32_t*)hash);
+    return str;
 }
+
+static int 
+ComparatorHash(const void *h1, const void *h2){
+    if      ( *(HASH_TYPE*)h1 < *(HASH_TYPE*)h2 ) return -1;
+    else if ( *(HASH_TYPE*)h1 > *(HASH_TYPE*)h2 ) return 1;
+    else return 0;
+}
+
+static int 
+ComparatorElasticBufItemByHashQSort(const void *p1, const void *p2){
+    return ComparatorHash( &((ElasticBufItemData*)p1)->key_hash,
+			   &((ElasticBufItemData*)p2)->key_hash );
+}
+
+#define TEST_CASE_1 "1"
+static int 
+Map(const char *data, size_t size, int last_chunk, Buffer *map_buffer ){
+    int unhandled_data_pos=0;
+    /*instead of parsing we will generate only some data depending on
+      first character in string */
+    switch(*data){
+    case '1':
+	/*do not change list below, tests are based on it*/
+	AddItemsStrKey( map_buffer, "hello", 0xf00001, 1, 5 );
+	AddItemsStrKey( map_buffer, "pello", 0x0f0001, 2, 21 );
+	AddItemsStrKey( map_buffer, "bello", 0x00f001, 3, 56 );
+	AddItemsStrKey( map_buffer, "good",  0x000f02, 4, 56 );
+	unhandled_data_pos=size; /*all data processed*/
+	break;
+    default:
+	break;
+    }
+    return unhandled_data_pos;
+}
+
+int Combine( const Buffer *map_buffer, Buffer *reduce_buffer ){
+    int combined_count=0;
+
+    ElasticBufItemData* current;
+    ElasticBufItemData* combine = alloca(map_buffer->header.item_size);
+    if ( map_buffer->header.count ){
+	GetBufferItem( map_buffer, 0, combine );
+    }
+
+    /*go through items of sorted map_buffer*/
+    for (int i=0; i < map_buffer->header.count; i++){
+	/*current loop item*/
+	current = (ElasticBufItemData*) BufferItemPointer( map_buffer, i );
+	/*if current item can be added into reduce_buffer*/
+	if ( *(HASH_TYPE*)&current->key_hash != *(HASH_TYPE*)&combine->key_hash ){
+ 	    /*save previously combined item*/
+	    AddBufferItem( reduce_buffer, combine );
+	    GetBufferItem( map_buffer, i, combine );
+	    combined_count=1;
+	}
+	/*if previous item and new one has the same key then we need 
+	 *to modify value of item to reduce it*/
+	else{
+	    ++combined_count;
+	    /*update value only for items that now combining more than one item*/
+	    if ( combined_count > 1 ){
+		combine->value.addr += current->value.addr;
+	    }
+	}
+    }
+
+    /*save last combined value if combined item not yet added*/
+    if (combined_count>0){
+	AddBufferItem( reduce_buffer, combine );
+    }
+    return 0;
+}
+
+/*functions related to histogram testing */
 
 static Buffer*
 TestHistogramGetDividers(struct MapReduceUserIf* mif, int count){
@@ -87,26 +149,26 @@ TestHistogramGetDividers(struct MapReduceUserIf* mif, int count){
     Buffer* divider_hashes = malloc(sizeof(Buffer));
     AllocBuffer( divider_hashes, sizeof(HASH_TYPE), 
 		 count /*exact count of dividers we want to produce*/ );
-    Buffer* current_buffer;
+    Buffer* current;
     int all_buffers_items_count=0;
+
     for(int i=0; i < count; i++){
 	ret = AllocBuffer( &histograms[i].buffer, sizeof(HASH_TYPE), GRANULARITY );
 	assert(!ret);
 
-	current_buffer = &array_of_buffers[i];
-	ret = AllocBuffer( current_buffer, ITEM_SIZE, GRANULARITY );
+	current = &array_of_buffers[i];
+	ret = AllocBuffer( current, ITEM_SIZE, GRANULARITY );
 	assert(!ret);
 
-	int buf_items_count = AddItemsIntoBuffer(current_buffer);
-	assert(buf_items_count== current_buffer->header.count);
-	all_buffers_items_count += buf_items_count;
+	MapInputDataLocalProcessing( mif, TEST_CASE_1, strlen(TEST_CASE_1), 1, current );
+	all_buffers_items_count += current->header.count;
 
-	int expected_items_in_histogram = buf_items_count/HISTOGRAM_STEP;
-	if ( buf_items_count > expected_items_in_histogram*HISTOGRAM_STEP )
+	int expected_items_in_histogram = all_buffers_items_count/HISTOGRAM_STEP;
+	if ( all_buffers_items_count > expected_items_in_histogram*HISTOGRAM_STEP )
 	    ++expected_items_in_histogram;
 
 	int histogram_items_count = GetHistogram( mif,
-						  current_buffer,
+						  current,
 						  HISTOGRAM_STEP,
 						  &histograms[i] );
 	/*GetHistogram result testing*/
@@ -129,12 +191,13 @@ int main(int argc, char** argv){
     int ret;
     struct MapReduceUserIf mif;
     PREPARE_MAPREDUCE( &mif, 
-		       NULL, /*map*/
-		       NULL, /*combine*/
+		       Map, /*map*/
+		       Combine, /*combine*/
 		       NULL, /*reduce*/
-		       HashComparator,
+		       ComparatorElasticBufItemByHashQSort,
+		       ComparatorHash,
 		       PrintableHash,
-		       0,    /*value addr is not data, use as pointer*/
+		       1,    /*value addr is data*/
 		       ITEM_SIZE, 
 		       sizeof(HASH_TYPE) );
     Buffer* dividers = NULL;
@@ -151,8 +214,9 @@ int main(int argc, char** argv){
 
     /*Pending tests*/
     /*Test histogram & dividers creation for 2 mappers & 2 reducers*/
-    //TestHistogramGetDividers(&mif, 2);
+    /* TestHistogramGetDividers(&mif, 2); */
     /*Test histogram & dividers creation for 3 mappers & 3 reducers*/
-    //TestHistogramGetDividers(&mif, 3);
+    /* TestHistogramGetDividers(&mif, 3); */
+
     return 0;
 }
