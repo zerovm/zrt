7c2e31add7867ee244397650d389e9a03e3dac0d
Constantine Peresypkin <constantine@litestack.com>
2013-04-07 15:35:42 -0500

    remove zerovm ports to new repo

--- libports/lcms-1.19.dfsg/debian/__init__.py
+++
@@ -1,3 +0,0 @@
-"""This file is used to make it possible to import lcms python extensions as
-'import lcms'
-"""
--- libports/tiff-3.9.5/conf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ZVM_TOOLCHAIN=${ZVM_SDK_ROOT}
-export PATH=${PATH}:${ZVM_TOOLCHAIN}/bin:
-
-CFLAGS="-I$PREFIX/include" LDFLAGS="-s -L$PREFIX/lib" ./configure --host=x86_64-nacl --enable-static --prefix=$PREFIX
